d7ea63b9cdc24ea4af93f2e88f57a85516faf84c
huyhuyshi <huy.lt02432@sinhvien.hoasen.edu.vn>
2025-08-23 15:00:58 +0700

    add profile

--- dimora-client/src/app/pages/profile/profile.component.ts
+++ dimora-client/src/app/pages/profile/profile.component.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import {MatTab, MatTabGroup} from "@angular/material/tabs";
+import {ScheduleComponent} from './schedule/schedule.component';
+import {MaterialModule} from '../../shared/material.module';
 
 @Component({
   selector: 'app-profile',
-    imports: [
-        MatTab,
-        MatTabGroup
-    ],
+  imports: [
+    MatTab,
+    MatTabGroup,
+    ScheduleComponent,
+    MaterialModule
+  ],
   templateUrl: './profile.component.html',
   styleUrl: './profile.component.scss'
 })
